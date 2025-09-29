a60c2caf67554363a13fdb1c2d3762914b563cd2
bangyugi <bangtranvan08@gmai.com>
2025-09-29 21:37:57 +0700

    add CartItemService

---
+++ src/main/java/com/bangvan/controller/CartItemController.java
@@ -0,0 +1,52 @@
+package com.bangvan.controller;
+
+import com.bangvan.dto.request.cart.UpdateCartItemRequest;
+import com.bangvan.dto.response.ApiResponse;
+import com.bangvan.service.CartItemService;
+import com.bangvan.service.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/cart-items")
+public class CartItemController {
+
+
+    private final CartItemService cartItemService;
+
+    @PutMapping("/{cartItemId}")
+    @Operation(summary = "Update item in cart", description = "Update the quantity of a product in the current user's shopping cart")
+    public ResponseEntity<ApiResponse> updateCartItem(
+            Principal principal,
+            @PathVariable Long cartItemId,
+            @Valid @RequestBody UpdateCartItemRequest request) {
+        ApiResponse apiResponse = ApiResponse.success(
+                HttpStatus.OK.value(),
+                "Item updated successfully",
+                cartItemService.updateCartItem(principal, cartItemId, request)
+        );
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{cartItemId}")
+    @Operation(summary = "Remove item from cart", description = "Remove an item from the current user's shopping cart")
+    public ResponseEntity<ApiResponse> removeCartItem(
+            Principal principal,
+            @PathVariable Long cartItemId) {
+        cartItemService.removeCartItem(principal, cartItemId);
+        ApiResponse apiResponse = ApiResponse.success(
+                HttpStatus.OK.value(),
+                "Item removed successfully",
+                null
+        );
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/bangvan/service/impl/CartItemServiceImpl.java
@@ -0,0 +1,106 @@
+package com.bangvan.service.impl;
+
+import com.bangvan.dto.request.cart.UpdateCartItemRequest;
+import com.bangvan.dto.response.cart.CartItemResponse;
+import com.bangvan.entity.Cart;
+import com.bangvan.entity.CartItem;
+import com.bangvan.entity.User;
+import com.bangvan.exception.AppException;
+import com.bangvan.exception.ErrorCode;
+import com.bangvan.exception.ResourceNotFoundException;
+import com.bangvan.repository.CartItemRepository;
+import com.bangvan.repository.CartRepository;
+import com.bangvan.repository.UserRepository;
+import com.bangvan.service.CartItemService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.security.Principal;
+
+@Service
+@RequiredArgsConstructor
+public class CartItemServiceImpl implements CartItemService {
+
+    private final UserRepository userRepository;
+    private final CartItemRepository cartItemRepository;
+    private final CartRepository cartRepository;
+    private final ModelMapper modelMapper;
+
+    @Transactional
+    @Override
+    public void removeCartItem(Principal principal, Long cartItemId) {
+        String username = principal.getName();
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+        Cart cart = cartRepository.findByUser(user)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart", "user", username));
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(() -> new ResourceNotFoundException("CartItem", "ID", cartItemId));
+        if (!cartItem.getCart().getId().equals(cart.getId())) {
+            throw new AppException(ErrorCode.ACCESS_DENIED);
+        }
+        cart.getCartItems().remove(cartItem);
+        cartItemRepository.delete(cartItem);
+        updateCartTotals(cart);
+    }
+
+    @Transactional
+    @Override
+    public CartItemResponse updateCartItem(Principal principal, Long cartItemId, UpdateCartItemRequest request) {
+        String username = principal.getName();
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+        Cart cart = cartRepository.findByUser(user)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart", "user", username));
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(() -> new ResourceNotFoundException("CartItem", "ID", cartItemId));
+        if (!cartItem.getCart().getId().equals(cart.getId())) {
+            throw new AppException(ErrorCode.ACCESS_DENIED);
+        }
+        cartItem.setQuantity(request.getQuantity());
+        cartItem.setPrice(cartItem.getProduct().getPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
+        cartItem.setSellingPrice(cartItem.getProduct().getSellingPrice().multiply(BigDecimal.valueOf(request.getQuantity())));
+        cartItemRepository.save(cartItem);
+        updateCartTotals(cart);
+
+        return modelMapper.map(cartItem, CartItemResponse.class);
+    }
+
+    private void updateCartTotals(Cart cart) {
+        BigDecimal totalPrice = BigDecimal.ZERO;
+        BigDecimal totalDiscountedPrice = BigDecimal.ZERO;
+        int totalItem = 0;
+
+        for (CartItem cartsItem : cart.getCartItems()) {
+            totalPrice = totalPrice.add(cartsItem.getPrice());
+            totalDiscountedPrice = totalDiscountedPrice.add(cartsItem.getSellingPrice());
+            totalItem += cartsItem.getQuantity();
+        }
+
+        cart.setTotalSellingPrice(totalDiscountedPrice);
+        cart.setTotalItem(totalItem);
+        cart.setDiscount(calculateDiscountPercentage(totalPrice, totalDiscountedPrice));
+
+        cartRepository.save(cart);
+    }
+
+    private BigDecimal calculateDiscountPercentage(BigDecimal totalPrice, BigDecimal totalDiscountedPrice) {
+
+        if (totalPrice == null || totalDiscountedPrice == null ||
+                totalPrice.compareTo(BigDecimal.ZERO) <= 0 ||
+                totalDiscountedPrice.compareTo(totalPrice) > 0) {
+            return BigDecimal.ZERO;
+        }
+        BigDecimal discount = totalPrice.subtract(totalDiscountedPrice);
+        BigDecimal discountPercentage = discount
+                .multiply(new BigDecimal("100"))
+                .divide(totalPrice, 2, RoundingMode.HALF_UP);
+        return discountPercentage;
+    }
+
+
+}
